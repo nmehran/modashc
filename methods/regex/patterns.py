69ac410b8e633f4e8e365dcb1ae1924a226cf80a
delta <nm@gradientdynamics.com>
2024-06-21 01:38:55 -0400

    update: regex command templates

--- methods/regex/patterns.py
+++ methods/regex/patterns.py
@@ -2,10 +2,11 @@ import re
 
 
 # A regex pattern with a 'command' placeholder to be formatted dynamically at runtime.
+# Matches a $() command substitution block, including nested parentheses
 COMMAND_TEMPLATE_PATTERN = (
     r'''\\"|"(?:\\"|[^"$])*"|\'(?:\\\'|[^\'])*\''''  # Search unquoted commands and command separators
     r'|(^|[|;&()\n{{}}]*?\s*)'  # Captures the start of the input or delimiters followed by optional whitespace
-    r'({command})'  # Captures the 'command' specified
+    r'(\b{command}\b)'  # Captures the 'command' specified
     r'((?:\s+'  # Matches leading whitespace before arguments
     r'(?:'  # Starts grouping for different argument types
     r'"(?:\\.|[^"\\])*"'  # Matches double-quoted strings with escaped characters
@@ -16,14 +17,21 @@ COMMAND_TEMPLATE_PATTERN = (
     r')'  # Captures all following arguments as a group
 )
 
+PATH_COMMAND_TEMPLATE_PATTERN = (
+    r'\$\(\s*\b{command}\b\s+(".*?"|\'.*?\'|[^)]+)\s*\)'
+)
 
-def create_command_pattern(command):
+
+def create_command_pattern(command, template=None):
+    if template is None:
+        template = COMMAND_TEMPLATE_PATTERN
+
     # Escape the command to handle any special characters it might contain
     escaped_command = re.escape(command)
 
     # Create a regex pattern dynamically based on the command
     pattern = re.compile(
-        COMMAND_TEMPLATE_PATTERN.format(command=escaped_command), re.DOTALL
+        template.format(command=escaped_command), re.DOTALL
     )
 
     return pattern
@@ -32,6 +40,28 @@ def create_command_pattern(command):
 # Regular expression to match source statements and global variable definitions
 # Example: source /path/to/file or . /path/to/file
 SOURCE_PATTERN = re.compile(r'(^|;\s*|\s*&{2}\s*|\$\(\s*)(source|\.)\s+([^\n#;]*)')
+# SOURCE_PATTERN = create_command_pattern(command='source')
+
+# Regex to match dirname command usage, handling nested and mismatched quotes
+# Example: $(dirname "/path/to/dir")
+DIRNAME_PATTERN = create_command_pattern(command='dirname', template=PATH_COMMAND_TEMPLATE_PATTERN)
+
+# Regex to match basename command usage, handling nested and mismatched quotes
+# Example: $(basename "/path/to/file")
+BASENAME_PATTERN = create_command_pattern(command='basename', template=PATH_COMMAND_TEMPLATE_PATTERN)
+
+# Regex to match realpath command usage, handling nested and mismatched quotes
+# Example: $(realpath "/path/to/dir")
+REALPATH_PATTERN = create_command_pattern(command='realpath', template=PATH_COMMAND_TEMPLATE_PATTERN)
+
+# Regex to capture set commands, ensuring they're not part of a comment or a string
+# Example: set -e or set +x
+SET_PATTERN = re.compile(r'^\s*set\s+([-\w\s]+)', re.MULTILINE)
+# SET_PATTERN = create_command_pattern(command='set')
+
+# Regex for cd commands, accommodating paths with optional quotes and surrounding whitespace
+# Example: cd /path/to/dir or cd "/path with spaces"
+CD_PATTERN = create_command_pattern(command='cd')
 
 # Regex to match bash variable declarations which can be used to define paths
 # Example: export VAR=value or VAR=value
@@ -50,26 +80,6 @@ VARIABLE_COMPLEX_PATTERN = re.compile(
     r')',
     re.MULTILINE
 )
-
-# Regex for cd commands, accommodating paths with optional quotes and surrounding whitespace
-# Example: cd /path/to/dir or cd "/path with spaces"
-CD_PATTERN = create_command_pattern(command='cd')
-
-# Regex to match dirname command usage, handling nested and mismatched quotes
-# Example: $(dirname "/path/to/dir")
-DIRNAME_PATTERN = re.compile(r'\$\(\s*dirname\s+(".*?"|\'.*?\'|[^)]+)\s*\)')
-
-# Regex to match basename command usage, handling nested and mismatched quotes
-# Example: $(basename "/path/to/file")
-BASENAME_PATTERN = re.compile(r'\$\(\s*basename\s+(".*?"|\'.*?\'|[^)]+)\s*\)')
-
-# Regex to match realpath command usage, handling nested and mismatched quotes
-# Example: $(realpath "/path/to/dir")
-REALPATH_PATTERN = re.compile(r'\$\(\s*realpath\s+(".*?"|\'.*?\'|[^)]+)\s*\)')
-
-# Regex to capture set commands, ensuring they're not part of a comment or a string
-# Example: set -e or set +x
-SET_PATTERN = re.compile(r'^\s*set\s+([-\w\s]+)', re.MULTILINE)
 
 # Regex to capture bash function definitions
 # Example: function my_func or my_func() {
